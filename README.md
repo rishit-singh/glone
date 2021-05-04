91296b4cb12ccfc10c4e4d220bdfbd36169654f7
rishit-singh <rsrishitsingh@gmail.com>
2021-05-04 18:34:54 +0530

    Updated README and install.sh.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # glone
 Just a quick git tool i created for my own convenience.
+
+## Dependencies
+- numpy	
+
+## Building
+
+###	Linux and macOS:
+`./install.sh`
+
+## Usage
+
+1. Edit the ~/.glone/config.json as put your username and git server URL.
+2. Run `glone <reponame>` to clone your repository.
+
--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 mkdir ~/.glone;
-cp ./glone.py ~/.glone/glone.py;
+
+echo "{ \"account\":\"\", \"scm_url\":\"\" }" >> ~/.glone/config.json;
+
+echo "rm -rf ~/.glone; sudo rm -f /bin/glone; rm -f remove.sh;" >> remove.sh;
+
+chmod +x ./remove.sh;
+
+printf "Build succeeded.\n"
 
 sudo gcc glone.c -o /bin/glone;
+
