c3267c1011603f275c3c87992b47f564620ef042
rishit-singh <rsrishitsingh@gmail.com>
2021-05-05 22:21:00 +0530

    Added numpt install statement.

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 mkdir ~/.glone;
 
+python3 -m pip install numpy;
+
+cp ./glone.py ~/.glone/glone.py;
+
 echo "{ \"account\":\"\", \"scm_url\":\"\" }" >> ~/.glone/config.json;
 
 echo "rm -rf ~/.glone; sudo rm -f /bin/glone; rm -f remove.sh;" >> remove.sh;
